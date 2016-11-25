84f01f442749d190d2321acdcc3aaf01ce4006f6
Elson <elson.dsouza44@gmail.com>
2016-11-25 14:06:16 +0530

    Added pug for rendering

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 app.use(express.static('front_end'))
-
 var path = require('path');
-
+var pug= require('pug');
+app.set('view engine', 'pug')
 
 var knex = require('knex')({
   client: 'mysql',
@@ -19,18 +19,22 @@ var server = app.listen(8080, function () {
 	var host = server.address().address
 	var port = server.address().port
 
-	console.log("Listening at http://%s:%s",host ,port)
+	console.log("\nListening at http://%s:%s\n",host ,port)
 })
 
 app.get('/',function(req,res){
-        res.sendFile(path.join(__dirname + '/front_end/index.html'));
+        //res.sendFile(path.join(__dirname + '/front_end/index.html'));
+        knex.select().from('flight').then(function(rows) {var html=pug.renderFile('front_end/index_test.pug',{rows:rows});
+                                                         res.send(html);
+                                                          console.log("Pug rendered\n");})
         })
     .get('/test', function(req, res) {
-		knex.select().from('flight').then(function(rows) {console.log(rows); res.send(rows);})
-		knex.select().from('flight_details').then(function(rows) {console.log(rows); res.send(rows);})
-		knex.select().from('credit_card_details').then(function(rows) {console.log(rows); res.send(rows);})
-		knex.select().from('location').then(function(rows) {console.log(rows); res.send(rows);})
-		knex.select().from('passenger_profile').then(function(rows) {console.log(rows); res.send(rows);})
-		knex.select().from('ticket_info').then(function(rows) {console.log(rows); res.send(rows);})
+
+//		knex.select().from('flight_details').then(function(rows) {console.log(rows); res.send(rows);})
+//		knex.select().from('credit_card_details').then(function(rows) {console.log(rows); res.send(rows);})
+//		knex.select().from('location').then(function(rows) {console.log(rows); res.send(rows);})
+//		knex.select().from('passenger_profile').then(function(rows) {console.log(rows); res.send(rows);})
+//		knex.select().from('ticket_info').then(function(rows) {console.log(rows); res.send(rows);})
+    res.send("Test");
 
 });
